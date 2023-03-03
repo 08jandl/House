d6d3028bb8830d78ea412168d7edf95905eb3d99
Silke Jandl <silke8421@gmx.at>
2023-03-03 08:12:21 +0100

    Changes made in class March 2nd

--- src/Divisible.java
+++ src/Divisible.java
@@ -2,25 +2,57 @@ import java.util.Scanner;
 
 public class Divisible {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter number: ");
-        int firstNumber = scanner.nextInt();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter number: ");
+//        int firstNumber = scanner.nextInt();
+//
+//        int secondNumber = 3;
+//
+//        int result = firstNumber % secondNumber;
+//
+//        if (result == 0) {
+//            System.out.println("The number is divisible by 3 ");
+//        } else {
+//            System.out.println("The number is not divisible by 3");
+//        }
+//
+//        int number = 2;
+//        //number += 2;
+//
+//        number = number + 2;
+//
+//        System.out.println(number);
 
-        int secondNumber = 3;
+        int dividend = 17;
+        int divisor = 9;
+        int resultOfDivisible = isDivisible(dividend, divisor);
 
-        int result = firstNumber % secondNumber;
-
-        if (result == 0) {
-            System.out.println("The number is divisible by 3 ");
-        } else {
-            System.out.println("The number is not divisible by 3");
+        if (resultOfDivisible != 0) {
+            System.out.println(dividend + " is divisible by " + divisor);
         }
 
-        int number = 2;
-        //number += 2;
+        int dividend2 = 12;
+        int divisor2 = 9;
+        int resultOfDivisible2 = isDivisible2(dividend2, divisor2);
 
-        number = number + 2;
+    }
 
-        System.out.println(number);
+    public static int isDivisible (int dividend, int divisor) {
+        int restFromDivision = dividend % divisor;
+
+        if (restFromDivision == 0) {
+            return dividend;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int isDivisible2 (int dividend, int divisor) {
+        int restFromDivision = dividend % divisor;
+        int returnValue = 0;
+        if (restFromDivision == 0) {
+            returnValue = dividend;
+        }
+        return returnValue;
     }
 }
